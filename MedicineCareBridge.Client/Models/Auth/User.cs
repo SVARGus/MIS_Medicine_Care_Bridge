870c65fc8825a9f519ebf2d814c485f3ab2db2bb
Ivan <ivansamiko1996@gmail.com>
2025-06-04 22:59:42 +0300

    Начало работы 04.06.25

---
+++ MedicineCareBridge.Client/Models/Auth/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineCareBridge.Client.Models.Auth
+{
+    public class User
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
