7a16c7cb0bcf204a645c95abd55d5b8b0a25d0dc
Ivan <ivansamiko1996@gmail.com>
2025-06-08 20:06:46 +0300

    Конец работы

--- MedicineCareBridge.Client/Helpers/Auth/RelayCommand.cs
+++ MedicineCareBridge.Client/Helpers/Auth/RelayCommand.cs
@@ -18,7 +18,12 @@ namespace MedicineCareBridge.Client.Helpers.Auth
             _canExecute = canExecute;
         }
 
-        public event EventHandler? CanExecuteChanged;
+       
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
 
         public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
 
