82479812e3027b6c0b7dc927e25b5028b5ee9409
Ivan <ivansamiko1996@gmail.com>
2025-06-14 20:09:39 +0300

    Начало работы над добавлением Юзера

---
+++ MedicineCareBridge.Client/Views/AddUser/AddUserControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MedicineCareBridge.Client.ViewModels.AddUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedicineCareBridge.Client.Views.AddUser
+{
+    /// <summary>
+    /// Логика взаимодействия для AddUserControl.xaml
+    /// </summary>
+    public partial class AddUserControl : UserControl
+    {
+        public AddUserControl()
+        {
+            InitializeComponent();
+            DataContext = new AddUserViewModel();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (DataContext is AddUserViewModel vm)
+            {
+                vm.Password = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
