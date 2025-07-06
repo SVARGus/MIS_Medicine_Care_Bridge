a428b56ad1ecff84d7443d6bc21a2356565582d4
SVARGus <kyznetsovpn@mail.ru>
2025-07-06 19:50:29 +0300

    Добавлен ServiceCollectionExtensions, позже перенести в сервер

---
+++ DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MedicineCareBridge.DataAccess.Repositories.Implementations;
+using MedicineCareBridge.DataAccess.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicineCareBridge.DataAccess.Extensions
+{
+    // Позже данный клас переесем в серверную часть проекта + appsettings.json со строкой подключения (пример ниже)
+    //{
+    //    "ConnectionStrings": 
+    //    {
+    //        "DefaultConnection": "Host=localhost;Port=5432;Database=MedicineCare;Username=youruser;Password=yourpass"
+    //    }
+    //}
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // 1) Регистрация DbContext
+            services.AddDbContext<MedicineCareBridgeDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            // 2) Регистрация репозиториев
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IPassportRepository, PassportRepository>();
+            services.AddScoped<IPermissionRepository, PermissionRepository>();
+            services.AddScoped<IPersonalDataRepository, PersonalDataRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<IRolePermissionRepository, RolePermissionRepository>();
+            services.AddScoped<ISnilsRepository, SnilsRepository>();
+            services.AddScoped<IInsuranceDocumentRepository, InsuranceDocumentRepository>();
+            services.AddScoped<IUniversalDocumentRepository, UniversalDocumentRepository>();
+            services.AddScoped<IUserDocumentTypeRepository, UserDocumentTypeRepository>();
+            services.AddScoped<IDocumentTypeRepository, DocumentTypeRepository>();
+
+            return services;
+        }
+    }
+}
